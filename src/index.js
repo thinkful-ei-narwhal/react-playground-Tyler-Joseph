12ce128ed4e457165172431d0337a951913d0f60
Joseph Rivera <josephr98@gmail.com>
2020-04-08 14:58:08 -0400

    working on accordion drill

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import HelloWorld from './state-drills/HelloWorld';
 import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
 import Bomb from './state-drills/Bomb';
 import * as serviceWorker from './serviceWorker';
 
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <HelloWorld />
-    <Bomb />
-    <RouletteGun />
+    <Accordion sections={sections}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+
+  this.props.sections.map(section => )
+
+  actions = () {
+    
+  }
+
+  render() {
+    console.log(this.props.sections.title);
+    return (
+      <ul>
+        <li>
+          <button>Hello</button>
+        </li>
+      </ul>
+    )
+  }
+};
