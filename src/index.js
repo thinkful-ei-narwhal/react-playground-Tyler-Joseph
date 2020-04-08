6d524089f1297bed7ff4464ff9d501ac04526470
Joseph Rivera <josephr98@gmail.com>
2020-04-08 11:54:02 -0400

    updated src

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloWorld from './state-drills/HelloWorld';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
